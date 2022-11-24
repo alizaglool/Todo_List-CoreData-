d59a126c23a705661e86e77f74494ee57f08ee54
alizaghloul <alizaglool90@gmail.com>
2022-11-24 11:43:12 +0200

    Initial Commit

---
+++ Todo_List(CoreData)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todo_List(CoreData)
+//
+//  Created by Zaghloul on 24/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
